c7c9b19eca712849e306255e399eb92e55099f87
dutarrr <duzguntutar@gmail.com>
2021-08-28 15:50:15 +0300

    android project init

---
+++ 1-Basic-Calculator/app/src/main/java/com/dutar/basiccalculator/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.dutar.basiccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun topla(view: View){
+
+    }
+
+    fun cikar (view: View){
+
+    }
+
+    fun carp(view: View){
+
+    }
+    fun bol(view: View){
+
+    }
+}
