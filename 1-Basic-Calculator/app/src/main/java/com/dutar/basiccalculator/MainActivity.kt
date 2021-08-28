70326f375c714ca594cfef03d638299562268c45
dutarrr <duzguntutar@gmail.com>
2021-08-28 18:42:39 +0300

    calculate process

--- 1-Basic-Calculator/app/src/main/java/com/dutar/basiccalculator/MainActivity.kt
+++ 1-Basic-Calculator/app/src/main/java/com/dutar/basiccalculator/MainActivity.kt
@@ -3,25 +3,56 @@ package com.dutar.basiccalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var sayi1: Int? = null
+    var sayi2: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun topla(view: View){
+    fun topla(view: View) {
+        sayi1 = editTextOne.text.toString().toIntOrNull()
+        sayi2 = editTextTwo.text.toString().toIntOrNull()
+
+        if (sayi1 != null || sayi2 != null)
+            textView.text = "Sonu. ${sayi1!! + sayi2!!}"
+        else
+            textView.text = "HATA!!!"
 
     }
 
-    fun cikar (view: View){
+    fun cikar(view: View) {
+        sayi1 = editTextOne.text.toString().toIntOrNull()
+        sayi2 = editTextTwo.text.toString().toIntOrNull()
 
+        if (sayi1 != null || sayi2 != null)
+            textView.text = "Sonu. ${sayi1!! - sayi2!!}"
+        else
+            textView.text = "HATA!!!"
     }
 
-    fun carp(view: View){
+    fun carp(view: View) {
+        sayi1 = editTextOne.text.toString().toIntOrNull()
+        sayi2 = editTextTwo.text.toString().toIntOrNull()
 
+        if (sayi1 != null || sayi2 != null)
+            textView.text = "Sonu. ${sayi1!! * sayi2!!}"
+        else
+            textView.text = "HATA!!!"
     }
-    fun bol(view: View){
 
+    fun bol(view: View) {
+        sayi1 = editTextOne.text.toString().toIntOrNull()
+        sayi2 = editTextTwo.text.toString().toIntOrNull()
+
+        if (sayi1 != null || sayi2 != null)
+            textView.text = "Sonu. ${sayi1!! / sayi2!!}"
+        else
+            textView.text = "HATA!!!"
     }
 }
