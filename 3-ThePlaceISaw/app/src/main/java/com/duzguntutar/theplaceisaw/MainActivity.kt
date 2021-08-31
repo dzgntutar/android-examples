e27296883a440ff00d71f751e0a80c491d2d8ec7
dutarrr <duzguntutar@gmail.com>
2021-08-31 16:37:54 +0300

    Menu created

--- 3-ThePlaceISaw/app/src/main/java/com/duzguntutar/theplaceisaw/MainActivity.kt
+++ 3-ThePlaceISaw/app/src/main/java/com/duzguntutar/theplaceisaw/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.duzguntutar.theplaceisaw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.duzguntutar.theplaceisaw.databinding.ActivityMainBinding
 import com.duzguntutar.theplaceisaw.databinding.ActivityPlaceDetailBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private  lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.place_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_place_item) {
+            val intent = Intent(this, PlaceDetailActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
